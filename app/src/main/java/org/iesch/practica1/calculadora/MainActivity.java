d88003c503e638c84286a490eef454d09b350a26
michel <mrubios@iesch.org>
2022-09-28 00:49:41 +0200

    Intoducir dobles a la hora de mostrar resultados para ser más específicos

--- app/src/main/java/org/iesch/practica1/calculadora/MainActivity.java
+++ app/src/main/java/org/iesch/practica1/calculadora/MainActivity.java
@@ -25,23 +25,120 @@ public class MainActivity extends AppCompatActivity {
 
         btnSuma.setOnClickListener(v -> {
             //Al hacer click queremos capturar el texto introducido
-            int num1 = 0;
-            int num2 = 0;
-            String numero1 = n1.getText().toString();
-            if (!numero1.isEmpty()){
-                num1 = Integer.parseInt(numero1);
-            }else{
-                num1 = 0;
+            double num1 = 0;
+            double num2 = 0;
+            try {
+                String numero1 = n1.getText().toString();
+                if (!numero1.isEmpty()){
+                    num1 = Integer.parseInt(numero1);
+                }else{
+                    num1 = 0;
+                }
+                String numero2 = n2.getText().toString();
+                if (!numero2.isEmpty()){
+                    num2 = Integer.parseInt(numero2);
+                }else{
+                    num2 = 0;
+                }
+
+                double result = num1 + num2;
+                resultado.setText(String.valueOf(result));
+
             }
-            String numero2 = n2.getText().toString();
-            if (!numero2.isEmpty()){
-                num2 = Integer.parseInt(numero2);
-            }else{
-                num2 = 0;
+            catch (Exception e)
+            {
+                Toast.makeText(this, "No introduzcas caracteres no numéricos", Toast.LENGTH_LONG).show();
             }
 
-            int result = num1 + num2;
-            resultado.setText(String.valueOf(result));
+
+        });
+
+        btnResta.setOnClickListener(v -> {
+            //Al hacer click queremos capturar el texto introducido
+            double num1 = 0;
+            double num2 = 0;
+            try {
+                String numero1 = n1.getText().toString();
+                if (!numero1.isEmpty()){
+                    num1 = Integer.parseInt(numero1);
+                }else{
+                    num1 = 0;
+                }
+                String numero2 = n2.getText().toString();
+                if (!numero2.isEmpty()){
+                    num2 = Integer.parseInt(numero2);
+                }else{
+                    num2 = 0;
+                }
+
+                double result = num1 - num2;
+                resultado.setText(String.valueOf(result));
+
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, "No introduzcas caracteres no numéricos", Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+
+        btnMultiplicacion.setOnClickListener(v -> {
+            //Al hacer click queremos capturar el texto introducido
+            double num1 = 0;
+            double num2 = 0;
+            try {
+                String numero1 = n1.getText().toString();
+                if (!numero1.isEmpty()){
+                    num1 = Integer.parseInt(numero1);
+                }else{
+                    num1 = 0;
+                }
+                String numero2 = n2.getText().toString();
+                if (!numero2.isEmpty()){
+                    num2 = Integer.parseInt(numero2);
+                }else{
+                    num2 = 0;
+                }
+
+                double result = num1 * num2;
+                resultado.setText(String.valueOf(result));
+
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, "No introduzcas caracteres no numéricos", Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+
+        btnDivision.setOnClickListener(v -> {
+            //Al hacer click queremos capturar el texto introducido
+            double num1 = 0;
+            double num2 = 0;
+            try {
+                String numero1 = n1.getText().toString();
+                if (!numero1.isEmpty()){
+                    num1 = Integer.parseInt(numero1);
+                }else{
+                    num1 = 0;
+                }
+                String numero2 = n2.getText().toString();
+                if (!numero2.isEmpty()){
+                    num2 = Integer.parseInt(numero2);
+                }else{
+                    num2 = 0;
+                }
+
+                double result = num1 / num2;
+                resultado.setText(String.valueOf(result));
+
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, "No introduzcas caracteres no numéricos", Toast.LENGTH_LONG).show();
+            }
 
 
         });
